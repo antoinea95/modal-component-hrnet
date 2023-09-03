a526874152cf9438925d9b0a48c55d8742429715
Antoine <102359550+antoinea95@users.noreply.github.com>
2023-09-03 17:46:02 +0200

    config build lib

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin()
+  ],
+  build: {
+        lib: {
+          entry: resolve(__dirname, 'src/index.js'),
+          formats: ['es']
+        },
+        copyPublicDir: false,
+        rollupOptions: {
+            external: ['react', 'react/jsx-runtime'],
+        }
+    }
 })
